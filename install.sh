b9c5566267f39b7f25bca1e88d2bec3e4a257baa
roufsyed <afrid704@gmail.com>
2022-04-25 00:58:33 +0530

    Added proper directories to files so can be executed from any dir

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # The below code creates a hard link for the dotfiles
-ln .bashrc ~/.bashrc
-ln .gitconfig ~/.gitconfig
-ln .inputrc ~/.inputrc
-ln .tmux.conf ~/.tmux.conf
-ln .ideavimrc ~/.ideavimrc
-ln config ~/.ssh/config
+ln ~/Github Repositories/dotFiles/.bashrc ~/.bashrc
+ln ~/Github Repositories/dotFiles/.gitconfig ~/.gitconfig
+ln ~/Github Repositories/dotFiles/.inputrc ~/.inputrc
+ln ~/Github Repositories/dotFiles/.tmux.conf ~/.tmux.conf
+ln ~/Github Repositories/dotFiles/.ideavimrc ~/.ideavimrc
+ln ~/Github Repositories/dotFiles/config ~/.ssh/config
